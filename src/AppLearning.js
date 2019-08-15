47df9e038da9d1b4dfd9b94e1c56c0cb4dcd1c1f
VAO61 <vao.frontend@gmail.com>
2019-08-15 15:41:43 +0300

    --L

--- src/AppLearning.js
+++ src/AppLearning.js
@@ -1,4 +1,4 @@
-import './styles/main.scss';
+import './assets/scss/style.scss';
 import React from 'react';
 import logo from './logo.svg';
 import TestComponentA from './components/learning/TestComponentA';
@@ -9,16 +9,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <Button onClick={() => alert(1)}>
-          Test button
-        </Button>
-        <Button className={'test-class'} active={true} onClick={() => alert(2)}>
-          Test button 2
-        </Button>
-        <Button className={'test-class'} active onClick={() => alert(3)}>
-          Test button 3
-        </Button>
-      <TestComponentA />
+      <Button onClick={() => alert(1)}>Test button 1</Button>
+      <Button className={'test-class'} active onClick={() => alert(3)}>
+        Test button 2
+      </Button>
+      {/* <TestComponentA /> */}
       {/* <TestComponentB /> */}
     </div>
   );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,19 @@
 import './Button.scss';
 import React from 'react';
 
-const Button = ({ className = '', active = false,  children, onClick = () => {} }) => (
-  <button className={`button button_${active ? 'red' : 'blue'} ${className}`} type={'button'} onClick={onClick}>
+const Button = ({
+  className = '',
+  active = false,
+  children,
+  onClick = () => {}
+}) => (
+  <button
+    className={`button button_${active ? 'sub-brand' : 'brand'} ${className}`}
+    type={'button'}
+    onClick={onClick}
+  >
     {children}
   </button>
-)
+);
 
 export default Button;
