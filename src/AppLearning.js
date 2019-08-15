aac399c4b9f57a33cec8d865e91454e6a7f4367c
Unknown <iwork.mail@mail.ru>
2019-08-15 13:04:14 +0300

    --L

--- src/AppLearning.js
+++ src/AppLearning.js
@@ -1,12 +1,23 @@
+import './styles/main.scss';
 import React from 'react';
 import logo from './logo.svg';
 import TestComponentA from './components/learning/TestComponentA';
-// import TestComponentB from './components/learning/TestComponentB';
+// import TestComponentB from './components/learning/TestComponentB'
+import Button from './components/Button/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <Button onClick={() => alert(1)}>
+          Test button
+        </Button>
+        <Button className={'test-class'} active={true} onClick={() => alert(2)}>
+          Test button 2
+        </Button>
+        <Button className={'test-class'} active onClick={() => alert(3)}>
+          Test button 3
+        </Button>
       <TestComponentA />
       {/* <TestComponentB /> */}
     </div>
