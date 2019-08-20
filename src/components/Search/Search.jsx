37222dea75e54e223f10a97414c398294ed5e453
VAO61 <vao.frontend@gmail.com>
2019-08-20 11:57:22 +0300

    0.0.4.3 _added axios

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,16 @@
-import './Search.scss'
+import './Search.scss';
 import React from 'react';
 import Select from '../Select/Select';
+import getJSON from '../../api/getJSON';
+
 //
 
-const Search = ({
-  className = '',
-}) => (
+const Search = ({ className = '' }) => (
   <section className={`${className} search`}>
     <div className="search__item">
-      <Select className="search__select" />{/* props */}
-        {/* <select className="form__select" v-model="typeValue" id="selectType">
+      <Select className="search__select" />
+      {/* props */}
+      {/* <select className="form__select" v-model="typeValue" id="selectType">
           <option
             className="form__option"
             v-for="option in typeList"
@@ -20,7 +21,8 @@ const Search = ({
       <label className="form__label" for="selectType">Type</label> */}
     </div>
     <div className="search__item">
-      <Select className="search__select" />{/* props */}
+      <Select className="search__select" />
+      {/* props */}
       {/*<select className="form__select" v-model="languageValue" id="selectLanguage">
          <option
           className="form__option"
@@ -32,7 +34,7 @@ const Search = ({
     <label className="form__label" for="selectLanguage">Language</label> */}
     </div>
     <div className="search__item">
-      <input className="search__input" type="text"/>
+      <input className="search__input" type="text" />
       {/* <input
         className="form__input"
         id="typeForSearch"
@@ -42,8 +44,8 @@ const Search = ({
     />
         <label className="form__label" for="typeForSearch">Type here for search</label> */}
     </div>
-    <button className="search__submit btn btn_brand">search</button >
-  </section >
-)
+    <button className="search__submit btn btn_brand">search</button>
+  </section>
+);
 
 export default Search;
