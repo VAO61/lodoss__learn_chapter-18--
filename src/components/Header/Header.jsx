97ee1b943c560ce5a0e534ac251db3dd173bd085
VAO61 <vao.frontend@gmail.com>
2019-08-20 11:28:43 +0300

    0.0.4.1 _Utility 'classNames'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
 import './Header.scss';
 import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 import GitHubSearchLogo from '../../assets/img/github-search-logo.svg';
+import classNames from 'classnames';
 
-
-const Header = ({
-  className = '',
-}) => (
-    <header className={`${className} header`}>
-      <Link to="/" className="link"><img class="header__logo" alt="header logo" src={GitHubSearchLogo}></img></Link>
+const Header = ({ className = '' }) => (
+  <header className={classNames(className, 'header')}>
+    <Link to="/" className="link">
+      <img class="header__logo" alt="header logo" src={GitHubSearchLogo} />
+    </Link>
     {/* <a href="/" class="link">
       <img class="header__logo" alt="header logo" src={GitHubSearchLogo} />
     </a> */}
     <div class="header__item header__control">
-        <NavLink to="/search" className="link link_tdn header__link">Search</NavLink>
+      <NavLink to="/search" className="link link_tdn header__link">
+        Search
+      </NavLink>
       {/* <a class="link link_tdn header__link header__link_active" href="/search">Search</a> */}
-        <NavLink to="/my-list" className="link link_tdn header__link">My list</NavLink>
+      <NavLink to="/my-list" className="link link_tdn header__link">
+        My list
+      </NavLink>
       {/* <a class="link link_tdn header__link" href="/my-list">My list</a> */}
     </div>
   </header>
-)
+);
 
 export default Header;
