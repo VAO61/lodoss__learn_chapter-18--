b2f1d39c923f3ba0c6f5ad061a344cb35d89f2d3
VAO61 <vao.frontend@gmail.com>
2019-08-21 18:08:56 +0300

    0.0.5.5

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,52 +1,42 @@
 import './Search.scss';
 import React from 'react';
 import Select from '../Select/Select';
-
-// import Select from 'react-select';
-// import getJSON from '../../api/getJSON';
-
-// const options = [
-//   { value: 'One', label: 'One' },
-//   { value: 'Two', label: 'Two' },
-//   { value: 'Three', label: 'Three' }
-// ];
-
-// const { selectedOption } = this.state;
+import getJSON from '../../api/getJSON';
 
 const Search = ({
-  className = ''
-  // state = {
-  //   selectedOption: null
-  // },
-  // handleChange = selectedOption => {
-  //   this.setState({ selectedOption });
-  //   console.log(`Option selected:`, selectedOption);
-  // }
+  className = '',
+  isDisableSearch = () => {
+    return (
+      this.searchValue === '' ||
+      this.typeValue === '' ||
+      this.languageValue === ''
+    );
+  },
+  getData = async () => {
+    if (this.isDisableSearch) {
+      return;
+    }
+
+    const { data } = await getJSON({
+      type: this.typeValue,
+      searchValue: this.searchValue,
+      lang: this.languageValue
+    });
+    this.$store.commit('updateItems', data.items);
+  }
+  // console.log('object')
 }) => (
   <section className={`${className} search`}>
     <div className="search__item">
-      {/* <Select
-        className="search__select"
-        options={[{ value: 'Repositories', label: 'Repositories' }]}
-        defaultValue="Repositories"
-        // defaultMenuIsOpen
-        defaultInputValue="Repositories"
-        placeholder="Type"
-      /> */}
       <Select
         options={[{ value: 'Repositories', label: 'Repositories' }]}
-        // TODO _!important: перестало передавать default value
         defaultValue="Repositories"
-        // defaultInputValue="Repositories"
         defaultInputValue="Repositories"
         placeholder="Type"
       />
     </div>
     <div className="search__item">
       <Select
-        // className="search__select"
-        // className="select"
-        // classNamePrefix="select"
         options={[
           { value: 'Javascript', label: 'Javascript' },
           { value: 'CSS', label: 'CSS' },
@@ -63,29 +53,16 @@ const Search = ({
         defaultInputValue=""
         placeholder="Language"
       />
-      {/* props */}
-      {/*<select className="form__select" v-model="languageValue" id="selectLanguage">
-         <option
-          className="form__option"
-          v-for="option in languageList"
-            : key="option"
-        :value="option"
-          >{{ option }}</option>
-        </select>
-    <label className="form__label" for="selectLanguage">Language</label> */}
     </div>
     <div className="search__item">
       <input className="search__input" type="text" />
-      {/* <input
-        className="form__input"
-        id="typeForSearch"
-        type="text"
-        v-model.trim="searchValue"
-      @keyup.enter="getData"
-    />
-        <label className="form__label" for="typeForSearch">Type here for search</label> */}
+      {/* <label className="form__label" for="typeForSearch">
+        Type here for search
+      </label>{' '} */}
     </div>
-    <button className="search__submit btn btn_brand">search</button>
+    <button className="search__submit btn btn_brand" disabled={isDisableSearch}>
+      search
+    </button>
   </section>
 );
 
