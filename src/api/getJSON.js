3e24e5666eda650f1da46be2c712278a7685313a
Unknown <iwork.mail@mail.ru>
2019-08-25 09:41:49 +0300

    0.0.8.1

--- src/api/getJSON.js
+++ src/api/getJSON.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const getJSON = function(type, searchValue, lang) {
   if (!type || !searchValue || !lang) {
-    throw new Error('Some Error text')
+    throw new Error('Some Error text');
   }
 
   return axios.get(
-    `https://api.github.com/search/${type}?q=${searchValue}+language:${lang}&sort=stars&order=desc`,
+    `https://api.github.com/search/${type.toLowerCase()}?q=${searchValue}+language:${lang}&sort=stars&order=desc`,
     {
       headers: { Accept: 'application/vnd.github.mercy-preview+json' }
     }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,14 +3,14 @@ import React, { useState, useCallback } from 'react';
 import Select from '../Select/Select';
 import getJSON from '../../api/getJSON';
 // import store from '../../store/store'
-import { updateSearchList } from '../../store/actions'
-import { connect } from 'react-redux'
+import { updateSearchList } from '../../store/actions';
+import { connect } from 'react-redux';
 
 const Search = ({ className = '', updateResultSearch }) => {
   // react-хуки
-  const [type, setType] = useState('Repositories')
-  const [lang, setLang] = useState('')
-  const [searchValue, setSearchValue] = useState('')
+  const [type, setType] = useState('Repositories');
+  const [lang, setLang] = useState('');
+  const [searchValue, setSearchValue] = useState('');
 
   // react-хуки
   const handleClickSearch = useCallback(async () => {
@@ -18,25 +18,28 @@ const Search = ({ className = '', updateResultSearch }) => {
     updateResultSearch(data.items);
     // dispatch(updateSearchList(data.items))
     // store.dispatch(updateSearchList(data.items))
-  }, [type, lang, searchValue])
+  }, [type, lang, searchValue]);
 
-  const handleChangeType = useCallback((data) => setType(data.value), [])
-  const handleChangeLang = useCallback((data) => setLang(data.value), [])
-  const handleChangeSearchValue = useCallback((e) => setSearchValue(e.target.value), [])
-  
+  const handleChangeType = useCallback(data => setType(data.value), []);
+  const handleChangeLang = useCallback(data => setLang(data.value), []);
+  const handleChangeSearchValue = useCallback(
+    e => setSearchValue(e.target.value),
+    []
+  );
+
   const isDisableSearch = type === '' || lang === '' || searchValue === '';
 
   return (
     <section className={`${className} search`}>
       <div className="search__item">
         <Select
-          options={[{ value: 'repositories', label: 'Repositories' }]}
+          options={[{ value: 'Repositories', label: 'Repositories' }]}
           value={type}
           // defaultValue={type}
           onChange={handleChangeType}
           defaultInputValue={type}
           placeholder="Type"
-          />
+        />
       </div>
       <div className="search__item">
         <Select
@@ -57,25 +60,35 @@ const Search = ({ className = '', updateResultSearch }) => {
           defaultInputValue={lang}
           // defaultInputValue=""
           placeholder="Language"
-          />
+        />
       </div>
       <div className="search__item">
-        
-        <input className="search__input" type="text" value={searchValue} onChange={handleChangeSearchValue}/>
+        <input
+          className="search__input"
+          type="text"
+          value={searchValue}
+          onChange={handleChangeSearchValue}
+        />
         {/* <label className="form__label" for="typeForSearch">
         Type here for search
       </label>{' '} */}
       </div>
-      <button className="search__submit btn btn_brand" disabled={isDisableSearch} onClick={handleClickSearch}>
+      <button
+        className="search__submit btn btn_brand"
+        disabled={isDisableSearch}
+        onClick={handleClickSearch}
+      >
         search
-    </button>
+      </button>
     </section>
-  )
+  );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  updateResultSearch: (array) => dispatch(updateSearchList(array))
-})
+const mapDispatchToProps = dispatch => ({
+  updateResultSearch: array => dispatch(updateSearchList(array))
+});
 
-
-export default connect(null, mapDispatchToProps)(Search);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Search);
